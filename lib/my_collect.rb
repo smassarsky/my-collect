cffe75abbac79ff1b160eb164e2d71d48ee5e086
smassarsky <seth.massarsky@gmail.com>
2020-09-30 16:28:56 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(collection)
+  i = 0
+  new_collections = []
+  while i < collection.length
+    new_collections << yield(collection[i])
+    i += 1
+  end
+  new_collections
+end
 
 
